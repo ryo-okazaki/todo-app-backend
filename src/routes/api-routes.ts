5eb681b78db5e242dfa13770f0fa0ec59b22b12c
Ryo Okazaki <okazaki3905@gmail.com>
2025-05-10 22:43:36 +0900

    add: routes

---
+++ src/routes/api-routes.ts
@@ -0,0 +1,25 @@
+import { Router } from 'express';
+import todoController from "../controllers/todo-controller";
+import userController from "../controllers/user-controller";
+import { authenticateToken } from '../middleware/auth-middleware';
+import { uploadMultiple } from '../middleware/upload-middleware';
+
+const apiRoutes = Router();
+const todoRoutes = Router();
+const userRoutes = Router();
+
+todoRoutes.get('', authenticateToken, todoController.getTodos);
+todoRoutes.get('/:id', authenticateToken, todoController.getTodos);
+todoRoutes.post('', authenticateToken, todoController.createTodo);
+todoRoutes.put('/:id', authenticateToken, uploadMultiple, todoController.updateTodo);
+// todoRoutes.delete('', todoController.deleteTodo);
+
+userRoutes.post('/login', userController.login);
+userRoutes.post('/register', userController.register);
+userRoutes.get('/verify/:token', userController.verifyToken);
+userRoutes.get('', authenticateToken, userController.get);
+
+apiRoutes.use('/todo', todoRoutes);
+apiRoutes.use('/user', userRoutes);
+
+export default apiRoutes;
