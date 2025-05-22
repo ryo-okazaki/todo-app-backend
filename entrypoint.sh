c3aa4ad85d7652e83c00cbce0ccb65ba7eb9e538
Ryo Okazaki <okazaki3905@gmail.com>
2025-05-22 21:52:15 +0900

    fix: アプリケーション以外のdocker関連のファイルを削除
    docker composeの設定を別リポジトリに移植するため

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # install npm packages
-cd /workdir && npm install
+cd /workdir/src && npm install
 
 # init prisma client
 npx prisma generate
@@ -9,8 +9,8 @@ npx prisma generate
 
 # start express server
 if [ "$EXPRESS_SERVER_DEBUG" = "1" ]; then
-  cd /workdir
+  cd /workdir/src
   npm install --save-dev nodemon && npm run debug-start
 else
-  cd /workdir && npm start
+  cd /workdir/src && npm start
 fi
