44be984f0197b6017fd936240dcb9f0b0bf54d76
Ryo Okazaki <okazaki3905@gmail.com>
2025-11-23 22:48:37 +0900

    fix: content output to the log

--- src/services/todo/create-service.ts
+++ src/services/todo/create-service.ts
@@ -4,7 +4,7 @@ import todoRepository from "../../repositories/todo-repository";
 class CreateTodoService
 {
   async handle(userId, request) {
-    console.log("Getting todos");
+    console.log("Creating todos");
 
     return await todoRepository.createTodo(userId, request);
   }
