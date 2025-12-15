fe4ca546b77e8531d42426376a82dfc20dc90cd0
Ryo Okazaki <okazaki3905@gmail.com>
2025-12-15 20:12:18 +0900

    fix: package.jsonの各種コマンドを修正

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,9 +7,4 @@ cd /workdir && npm install
 npx prisma generate
 
 # start express server
-if [ "$EXPRESS_SERVER_DEBUG" = "1" ]; then
-  cd /workdir/src
-  npm install --save-dev nodemon && npm run debug-start
-else
-  cd /workdir/src && npm start
-fi
+npm run dev
